a6f9b5dbbab7e27a637ca710bfb3572bfe796e34
mayukh-chr <mayukhsaha.private@gmail.com>
2024-11-01 17:40:37 +0530

    Listing 87

--- material.h
+++ material.h
@@ -68,7 +68,7 @@ class dielectric : public material {
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract)
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
             direction = refract(unit_direction, rec.normal, ri);
@@ -80,5 +80,11 @@ class dielectric : public material {
   private:
     
     double refraction_index;
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
 };
 #endif
